78e6651c253dd173d37c83b150d70a206648cfc6
Nuo (Tony) Chen <chen2886@purdue.edu>
2020-03-07 10:34:13 -0500

    Date: Sat Mar  7 10:34:13 EST 2020, changes: Updated commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 message="$@"
-echo "Message is `date` $message"
+echo "Message is Date: `date`, changes: $message"
 git add .
-git commit -m "$message"
+git commit -m "Date: `date`, changes: $message"
 git push origin dev
